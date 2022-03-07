6eca0294129fa4d565ef5d17d2dd51047fdc30bb
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-03-06 21:12:29 -0500

    Create jumpSetup.bash

---
+++ AssesmentScripts/jumpSetup.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Storyline: Setup Jump
+function NetCon() {
+conf="00-installer-config.yaml"
+#conf = "/etc/netplan/00-installer-config.yaml"
+#rm conf
+ip=192.168.0.1
+netmask=29
+gateway=172.16.50.2
+ns=172.16.200.11
+#echo the contents onto a file
+echo "
+network:
+   version: 2
+   renderer: networkd
+   ethernets:
+      ens160:
+         dhcp4: false
+         addresses: [${ip}/${netmask}]
+         gateway4: ${gateway}
+         nameservers:
+           addresses: [${ns}]
+" >> ${conf}
+netplan apply
+}
+function SysCon() {
+hostname="jump-sarah"
+#command to set hostname
+hostname ${hostname}
+}
+SysCon
+NetCon
